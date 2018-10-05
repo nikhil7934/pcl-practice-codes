b3a93841d6606d84777c32f93717cf5e396f96b3
atheti.nikhilendra <atheti.nikhilendra@students.iiit.ac.in>
2018-10-05 20:26:19 +0530

    added some more files

--- src/read_pcd.cpp
+++ src/read_pcd.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	pcl::PointCloud<pcl::PointXYZ>::Ptr pcloud (new pcl::PointCloud<pcl::PointXYZ>);
-	if (pcl::io::loadPCDFile<pcl::PointXYZ> ("../samples/test_pcd_01.pcd", *pcloud) == -1)
+	if (pcl::io::loadPCDFile<pcl::PointXYZ> ("test_pcd_01.pcd", *pcloud) == -1)
 	{
     	PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
     	return (-1);
